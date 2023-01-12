bd0b86bf681062102dfe15cbacd4289d5f1d4ab3
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-01-12 23:40:55 +0100

    [Enhancement] Add Initial Source Code (#4)
    * Create fresh Scriv fragment
    
    * Update 20230112_213219_41898282+github-actions[bot]_src.rst
    
    * Create .gitkeep
    
    * Create strmem.h
    
    * Update strmem.h
    
    * Update strmem.h
    
    * Create constants.c
    
    * Create stralloc.c
    
    * Update strmem.h
    
    * Update 20230112_213219_41898282+github-actions[bot]_src.rst
    
    * Update .bumpversion.cfg
    
    * Update strmem.h
    
    * Update and rename stralloc.c to strlmalloc.c
    
    * Update strmem.h
    
    * Create stralloc.c
    
    * Update strmem.h
    
    * Update and rename strlmalloc.c to strldmalloc.c
    
    * Update 20230112_213219_41898282+github-actions[bot]_src.rst
    
    * Create ci.yml
    
    * Update stralloc.c
    
    * Update stralloc.c
    
    * Update 20230112_213219_41898282+github-actions[bot]_src.rst
    
    * Update README.md
    
    * Update ci.yml
    
    * Delete cffconvert.yml
    
    * Update 20230112_211308_41898282+github-actions[bot]_configuration.rst
    
    * Update README.md
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/stralloc.c
@@ -0,0 +1,30 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2023 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+/// \file stralloc.c
+
+#include "strmem.h"
+
+inline char * stralloc (const char * const string)
+{
+  char * const buffer = strldmalloc (string);
+  return buffer != NULL ? strcpy (buffer, string) : NULL;
+}
+
+/******************************************************************************/
---
+++ src/strmem.h
@@ -0,0 +1,115 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2023 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+/**
+ * \author      Kevin Matthes
+ * \brief       The main header of this library.
+ * \copyright   GPL-3.0
+ * \date        2023
+ * \file        strmem.h
+ * \note        See `LICENSE' for full license.
+ *              See `README.md' for project details.
+ */
+
+/******************************************************************************/
+
+/**
+ * \brief   Prevent this header from being included twice.
+ *
+ * In case this header file should be included more than just once, unexpected
+ * side effects might take place.  This unintended behaviour will be avoided by
+ * the definition of this macro.
+ */
+
+#pragma once
+#ifndef __STRMEM_H__
+#define __STRMEM_H__
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+/**
+ * \author      Kevin Matthes
+ * \brief       This library's version.
+ * \copyright   GPL-3.0
+ * \date        2023
+ *
+ * This constant holds the version of this library for future reference.
+ */
+
+extern const char * const strmem_version;
+
+
+
+/**
+ * \author      Kevin Matthes
+ * \brief       Create a deep copy of the given string.
+ * \copyright   GPL-3.0
+ * \date        2023
+ * \param       string   The string to create a deep copy from.
+ * \returns     The pointer to allocated memory region.
+ *
+ * This function will create a deep copy of the given string.  As a result, a
+ * pointer to the allocated memory region will be returned.  If the allocation
+ * should fail for some reasons, `NULL` will be returned instead.
+ *
+ * The name `stralloc` originates from its purpose, the ***alloc***ation of
+ * ***str***ings.
+ */
+
+extern char * stralloc (const char * const string);
+
+
+
+/**
+ * \author      Kevin Matthes
+ * \brief       Allocate memory for a new string.
+ * \copyright   GPL-3.0
+ * \date        2023
+ * \param       string   The string whose length shall be overtaken.
+ * \returns     The pointer to allocated memory region.
+ *
+ * This function will allocate memory for a new string.  This is a utility
+ * function to be called by `stralloc` in order to create a deep copy of the
+ * given string.
+ *
+ * The memory region to be allocated will have a size of the length of the
+ * given string plus one.  The size of the given string is determined by its
+ * count of printable characters.  In order to match the C convention of string
+ * termination by zero, an extra character slot needs to be allocated, as well.
+ *
+ * The result of this function will be the pointer to the memory region which
+ * was allocated.  In case that the allocation should fail, the result will be
+ * `NULL`.
+ *
+ * The name `strldmalloc` originates from its purpose, the ***m***emory
+ * ***alloc***ation for ***str***ings with after a preceding ***l***ength
+ * ***d***etermination.
+ */
+
+extern char * strldmalloc (const char * const string);
+
+
+
+#endif // ! __STRMEM_H__
+
+/******************************************************************************/
