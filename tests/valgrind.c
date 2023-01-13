697a95ed3ff23efbb760fa19a9246a3fc514059c
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-01-13 19:07:36 +0100

    Create valgrind.c

---
+++ tests/valgrind.c
@@ -0,0 +1,51 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2023 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+/**
+ * \file valgrind.c
+ */
+
+/******************************************************************************/
+
+#include <stdlib.h>
+#include "../src/strmem.h"
+
+/**
+ * \author      Kevin Matthes
+ * \brief       The memory management check.
+ * \copyright   GPL-3.0
+ * \date        2023
+ * \returns     Zero.
+ *
+ * This is the main function to be analysed by Valgrind in order to check the
+ * memory management of the library functions.
+ */
+
+int main (void)
+{
+  char * const buffer = stralloc ("valgrind");
+  free (buffer);
+
+  buffer = strldmalloc ("another text");
+  free (buffer);
+
+  return 0;
+}
+
+/******************************************************************************/
