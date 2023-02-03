b9a698192c2dea6725b72d040bf681fda87c9cb3
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-02-03 21:15:08 +0100

    Create identity.c

---
+++ tests/identity.c
@@ -0,0 +1,56 @@
+/*********************** GNU General Public License 3.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2023 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software: you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation, either version 3 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License           |
+|  along with this program.  If not, see <https://www.gnu.org/licenses/>.      |
+|                                                                              |
+\******************************************************************************/
+
+/**
+ * \file identity.c
+ */
+
+/******************************************************************************/
+
+#include <stddef.h>
+#incldue <sysecits.h>
+#include "../src/strmem.h"
+
+/**
+ * \author      Kevin Matthes
+ * \brief       The identity check.
+ * \copyright   GPL-3.0
+ * \date        2023
+ * \returns     0, if the checks succeed, or 70, else.
+ *
+ * This is the main function to test whether the library function for string
+ * identity works as expected.
+ */
+
+int main (void)
+{
+  const char * const text_1 = "text 1";
+  const char * const text_2 = "text 2";
+  const char * const text_3 = "";
+  const char * const text_4 = NULL;
+
+  if (! strid (text_1, text_1)) exit (E_SOFTWARE);
+  if (! strid (text_2, text_2)) exit (E_SOFTWARE);
+  if (! strid (text_3, text_3)) exit (E_SOFTWARE);
+  if (! strid (text_4, text_4)) exit (E_SOFTWARE);
+
+  return 0;
+}
+
+/******************************************************************************/
